45d182c69461f40b79088c9d08128019b8d01437
Rohit Pawar <161442114+rohitbpawar25@users.noreply.github.com>
2025-05-08 23:05:46 +0530

    Delete Assignment_1/Assignment1_10.py

--- Assignment_1/Assignment1_10.py
+++
@@ -1,9 +0,0 @@
-# Write a program that accepts a name from the user and displays its length.
-
-def Length(name):
-    l = len(name)
-    return l
-print("Enter a Name :")
-Char = input()
-Value = Length(Char)
-print("length of string is :",Value)
