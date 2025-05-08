9c701f5a0dc749c8c3c5f3658664f8ffb11cc8fe
Rohit Pawar <161442114+rohitbpawar25@users.noreply.github.com>
2025-05-09 02:32:27 +0530

    Update Assignment1_2.py

--- Assignment_1/Assignment1_2.py
+++ Assignment_1/Assignment1_2.py
@@ -1,5 +1,7 @@
 # write a program that contains a function named ChkNum(), which accepts one number as a parameter. If the number is even, the function should display "Even number", otherwise "Odd number"
 
+
+
 def ChkNum(num):
     if num % 2 == 0:
         print("Even number")
@@ -7,34 +9,23 @@ def ChkNum(num):
         print("Odd number")
 
 ChkNum(11)  
-ChkNum(8)  
+ChkNum(8) 
 
-''' def ChkNum(number):
-    if number % 2 == 0:
-        return "Even number"
-    else:
-        return "Odd number"
+
+'''
+def ChkNum(number):
+   if number % 2 == 0:
+    return "Even number"
+   else:
+     return "Odd number"
     
 def main():
-    value = int(input("Enter a number: "))
+    print("Enter a number: ")
+    value = int(input())
     result = ChkNum(value)
     print(" Answer : ", result)
 
 if __name__ == "__main__":
     main()
-
-'''
-
-'''
-def ChkNum(number):
-    if number % 2 == 0:
-        return "Even number"
-    else:
-        return "Odd number"
-
-value = int(input("Enter a number: "))
-result = ChkNum(value)
-
-print(" Answer : ", result)
 
 '''
