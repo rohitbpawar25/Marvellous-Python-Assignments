647aaf183e6ff9c0e3ab462e2a59b354dd5eb219
Rohit Pawar <161442114+rohitbpawar25@users.noreply.github.com>
2025-05-09 01:43:57 +0530

    Delete Assignment_1 directory

--- Assignment_1/Assignment1_4.py
+++
@@ -1,7 +0,0 @@
-# Write a program which displays 'Marvellous' five times on the screen
-
-def Display():
-    for _ in range(5):  # Loop runs 5 times
-        print("Marvellous")
-
-Display()
--- Assignment_1/Assignment1_9.py
+++
@@ -1,7 +0,0 @@
-# Write a program that prints the first 10 even numbers.
-
-def display_even_numbers():
-    for i in range(2, 21, 2):
-        print(i, end=" ")
-
-display_even_numbers()
