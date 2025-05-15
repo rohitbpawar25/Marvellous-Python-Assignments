3041533ee24e56e157f87fdc636ecdbe5a3ec6af
Rohit Pawar <161442114+rohitbpawar25@users.noreply.github.com>
2025-05-15 20:27:06 +0530

    Add files via upload

---
+++ Assignment_04/Assignment4_01.py
@@ -0,0 +1,6 @@
+# Write a Python program that contains one lambda function which accepts one parameter and returns its power of two.
+
+Power = lambda Num1 : Num1 ** 2
+
+print("Answer",Power(4))
+print("Answer",Power(6))
---
+++ Assignment_04/Assignment4_02.py
@@ -0,0 +1,7 @@
+# Write a Python program that contains one lambda function which accepts two parameters and returns their multiplication.
+
+Multiplication = lambda Num1,Num2 : Num1 * Num2
+
+No1 = Multiplication(2,4)
+
+print("Multiplication is :",No1)
