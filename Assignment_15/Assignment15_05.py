1faa89be944261a204e6d2140336cc8ef5c5efca
Rohit Pawar <161442114+rohitbpawar25@users.noreply.github.com>
2025-06-17 00:36:23 +0530

    Add files via upload

---
+++ Assignment_15/Assignment15_05.py
@@ -0,0 +1,19 @@
+# Accept a file name and one string from the user and return the frequency of that string from the file.
+
+import os
+import sys
+
+def String_Frequency(FileName, String):
+    fobj = open(FileName, 'r')
+    Data = fobj.read()
+    fobj.close() 
+    frequency = Data.count(String)
+    print(f"The word '{String}' occurred {frequency} times in '{FileName}'.")
+
+def main():
+    Filename = input("Enter the Filename: ")
+    String = input("Enter the String: ")
+    String_Frequency(Filename, String)
+
+if __name__ == "__main__":
+    main()
