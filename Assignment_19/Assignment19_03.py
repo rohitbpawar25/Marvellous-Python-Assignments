59db2d6281a8707dbe684805d115248e8d37581b
Rohit Pawar <161442114+rohitbpawar25@users.noreply.github.com>
2025-06-21 00:24:31 +0530

    Update Assignment19_03.py

--- Assignment_19/Assignment19_03.py
+++ Assignment_19/Assignment19_03.py
@@ -37,7 +37,7 @@ def main():
 
     Border = "-" * 90
     print(Border + "\n")
-    print(f"{'Assignment19_02'.center(90)}\n")
+    print(f"{'Assignment19_03'.center(90)}\n")
     print(Border + "\n")
 
     if len(sys.argv) == 2:
