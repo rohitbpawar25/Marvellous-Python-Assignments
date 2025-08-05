a9339faa4228eb5c528d38d608000a65ffd9148c
Rohit Pawar <161442114+rohitbpawar25@users.noreply.github.com>
2025-08-05 12:20:22 +0530

    Delete Marvellous_class/PythonDSA directory

--- Marvellous_class/PythonDSA/Program039.py
+++
@@ -1,9 +0,0 @@
-def main():
-    Arr = [10,20,30,40,50]
-
-    no = 0
-    for no in Arr:  # for(no : Arr) {}
-        print(no)
-    
-if __name__ == "__main__":
-    main()
--- Marvellous_class/PythonDSA/Program055.py
+++
@@ -1,9 +0,0 @@
-def main():
-    print("Enter string : ")
-    str = input()
-
-    for ch in str:
-        print(ch)
-    
-if __name__ == "__main__":
-    main()
